43f11c846fde6ccfafa1febb0a73c20b404d8d94
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-03-07 10:36:37 -0300

    Update index2.js
    Criando o servidor e o listener

--- 1-node-farm/starter/index2.js
+++ 1-node-farm/starter/index2.js
@@ -1,6 +1,6 @@
 /* API DE ABACATE */
 
-/* Require das ferramentas necessarias. 
+/* 1. Require das ferramentas necessarias. 
 1. filesystem
 2. url
 3. http*/
@@ -8,3 +8,15 @@
 const fs = require("fs");
 const url = require("url");
 const http = require("http");
+
+/* 2. Criar um servidor utilizando http e create server numa const
+3. utilizar req url para capturar a url
+4. iniciar o server utilizando um listen, porta, endereço e FA */
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
