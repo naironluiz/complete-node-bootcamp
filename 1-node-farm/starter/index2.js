0ad3d1c6de9d3d8190f8dd490d75106cc75bf51e
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-03-07 10:33:51 -0300

    Update index2.js
    Requires necessarios

---
+++ 1-node-farm/starter/index2.js
@@ -0,0 +1,10 @@
+/* API DE ABACATE */
+
+/* Require das ferramentas necessarias. 
+1. filesystem
+2. url
+3. http*/
+
+const fs = require("fs");
+const url = require("url");
+const http = require("http");
