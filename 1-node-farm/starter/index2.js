4ce22c0bfac9046637392cb5f3fb91e6d6eac7a4
nairon.luiz <62431107+naironluiz@users.noreply.github.com>
2022-03-08 09:47:58 -0300

    Update index2.js
    adicionando uma função que vai substituir os templates no index

--- 1-node-farm/starter/index2.js
+++ 1-node-farm/starter/index2.js
@@ -29,7 +29,19 @@ const tempProduct = fs.readFileSync(
   "utf-8"
 );
 
-const replaceTemplate = (temp, product) => {};
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+};
 
 /* 3. Criar um servidor utilizando http e create server numa const
 4. utilizar req url para capturar a url
